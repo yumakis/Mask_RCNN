0868421c3da31cb6cdfbf46434a4d8dc403eb931
Térence Lee-Chao-Shit <yumakis@github.com>
2019-12-12 18:44:50 +0100

    correction

--- samples/goal.py
+++ samples/goal.py
@@ -140,8 +140,7 @@ class GoalDataset(utils.Dataset):
                 image_id=a['filename'],  # use file name as a unique image id
                 path=image_path,
                 width=width, height=height,
-                polygons=polygons,
-                class_ids=class_ids)
+                polygons=polygons)
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
